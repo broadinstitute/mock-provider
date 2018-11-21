f133e44cace52a445222208022e3c18cc9c97d15
Greg Polumbo <gpolumbo@broadinstitute.org>
2018-11-21 11:30:18 -0500

    GAWB-3807 Bond GETs /user/.well-known/openid-configuration as a status endpoint so we need this service to respond to that

--- retrieveKey.js
+++ retrieveKey.js
@@ -2,14 +2,17 @@ const { getObject } = require('./googleObjects');
 
 function retrieveKeyHandler(req, res) {
     if (req.method === "POST") {
-        const serviceAcctKeyUrl = "https://storage.googleapis.com/wb-dev-mock-provider/fake-sa-key.json";
+        const serviceAcctKeyUrl = "https://storage.googleapis.com/wb-dev-mock-provider/shared_sa_key.json";
         getObject(serviceAcctKeyUrl).then((key) => {
             res.status(200).send(key);
         }).catch((err) => {
             res.status(500).send({error: err});
         });
+    } else if (req.method === "GET" && req.path === "/.well-known/openid-configuration" ) {
+        // TODO: we can actually have this get the well-known config from the bucket
+        res.status(200).send(req.path + "\nThis URL is called by Bond to check the status of this Provider.  To get the well-known openid-config for this service, please go to: https://storage.googleapis.com/wb-dev-mock-provider/well-known.json");
     } else {
-        res.status(405).send({error: "Request method " + req.method + " is not permitted.  Method must be POST"});
+        res.status(400).send({error: "Unrecognized request.  POST to this url to get the Service Account Key.  Or issue a GET request with the path '/.well-known/openid-configuration'"});
     }
 }
 
