614d32ad93076652673931bd247ae636813fd576
Greg Polumbo <36171087+gpolumbo-broad@users.noreply.github.com>
2018-11-26 10:52:35 -0500

    GAWB-3807 Bond GETs /user/.well-known/openid-configuration as a status endpoint so we need this service to respond to that (#2)

--- retrieveKey.js
+++ retrieveKey.js
@@ -1,16 +1,28 @@
 const { getObject } = require('./googleObjects');
 
+function returnSharedServiceAccountKey(res) {
+    const serviceAcctKeyUrl = "https://storage.googleapis.com/wb-dev-mock-provider/shared_sa_key.json";
+    getObject(serviceAcctKeyUrl).then((key) => {
+        res.status(200).send(key);
+    }).catch((err) => {
+        res.status(500).send({error: err});
+    });
+}
+
+function returnOpenIdConfig(res) {
+    const openIdConfigUrl = "https://storage.googleapis.com/wb-dev-mock-provider/well-known.json";
+    getObject(openIdConfigUrl).then((conf) => {
+        res.status(200).send(conf);
+    }).catch((err) => {
+        res.status(500).send({error: err});
+    });
+}
+
 function retrieveKeyHandler(req, res) {
     if (req.method === "POST") {
-        const serviceAcctKeyUrl = "https://storage.googleapis.com/wb-dev-mock-provider/shared_sa_key.json";
-        getObject(serviceAcctKeyUrl).then((key) => {
-            res.status(200).send(key);
-        }).catch((err) => {
-            res.status(500).send({error: err});
-        });
+        returnSharedServiceAccountKey(res);
     } else if (req.method === "GET" && req.path === "/.well-known/openid-configuration" ) {
-        // TODO: we can actually have this get the well-known config from the bucket
-        res.status(200).send(req.path + "\nThis URL is called by Bond to check the status of this Provider.  To get the well-known openid-config for this service, please go to: https://storage.googleapis.com/wb-dev-mock-provider/well-known.json");
+        returnOpenIdConfig(res);
     } else {
         res.status(400).send({error: "Unrecognized request.  POST to this url to get the Service Account Key.  Or issue a GET request with the path '/.well-known/openid-configuration'"});
     }
